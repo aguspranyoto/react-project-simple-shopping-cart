ac316362f51b27132b7075a2dec3e870edcb89b5
aguspranyoto <agusprnyt@gmail.com>
2023-09-25 06:47:56 +0700

    update navbar home page url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/">Shop</Link>
+        <Link to="/react-project-simple-shopping-cart">Shop</Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
